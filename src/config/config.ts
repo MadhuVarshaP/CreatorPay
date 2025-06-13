514681726f33e139552575ec85565bac8541bba0
Madhu Varsha <madhuvarsha0608@gmail.com>
2025-06-13 22:57:44 +0530

    config file deleted

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,11 @@
 import { http, createConfig } from 'wagmi'
-import { base, mainnet } from 'wagmi/chains'
+import { base, baseSepolia, mainnet } from 'wagmi/chains'
 import { injected, metaMask, safe, walletConnect } from 'wagmi/connectors'
 
 const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string;
 
 export const config = createConfig({
-  chains: [mainnet, base],
+  chains: [mainnet, base, baseSepolia],
   connectors: [
     injected(),
     walletConnect({ projectId }),
@@ -15,5 +15,6 @@ export const config = createConfig({
   transports: {
     [mainnet.id]: http(),
     [base.id]: http(),
+    [baseSepolia.id]: http(),
   },
 })
